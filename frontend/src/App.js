56a94813461d20ea0cf2f742b530cc0e52c583f2
pavankumar <pavankumar19101999@gmail.com>
2026-03-08 09:09:01 +0530

    Updated hacker UI design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Login from "./Login";
-import Signup from "./Signup";
-import Dashboard from "./Dashboard";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <Router>
       <Routes>
+
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
+
       </Routes>
     </Router>
   );
